3f80d3d65c9c9de2f472d21711d036c12dae3224
C. Rafael Oliveira <crafaelsdo@gmail.com>
2024-04-19 16:59:50 -0300

    Atualização Automática
    Finalizei a lógica em python que atualiza o html com os estoques e preços dos produtos.

--- update.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("index.html", encoding = "utf-8") as html:
-    soup = BeautifulSoup(html, "html.parser")
-
-tbody = soup.find("tbody", {"id": "info-produtos"})
-
-print(list(filter(lambda x: x != "\n", tbody.children)))
